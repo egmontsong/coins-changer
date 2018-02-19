77025eb16a55949aaeca3895013e36795a783bdc
Tianlong Song <tianlongs@student.unimelb.edu.au>
2018-02-20 10:45:44 +1100

    added a table to record previous solution

--- coin_changer/change_maker.py
+++ coin_changer/change_maker.py
@@ -23,4 +23,29 @@ class ChangeMaker(object):
 
         """
         
-        return 'This is changes'
+        if self.target_amount < 0:
+            return 'Target amount must be positive.'
+            
+        # assign 'inf' to all cells to make sure they are greater than i
+        # min_combos[i] is smallest set of coins to target i.
+        min_combos = [0] + [float('inf')] * self.target_amount
+
+        # to memorise the all previous best combos, a table is used to record
+        # the result.
+        # combos[i][j] is the number of coins for coin value j to target i
+        combos = [[0] * len(self.coins)] * (self.target_amount + 1)
+
+        # start from 1 as 0 is already solved
+        for i in range(1, self.target_amount + 1):
+            for j, coin in enumerate(self.coins):
+                if coin > i:
+                    break
+                last = i - coin
+                combo = combos[last]
+                if (min_combos[i] > min_combos[last] + 1):
+                    min_combos[i] = min_combos[last] + 1
+                    current_combo = list(combo)
+                    current_combo[j] += 1
+                    combos[i] = current_combo
+
+        return combos[self.target_amount]
